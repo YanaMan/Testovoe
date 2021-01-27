58d243a872eeaa5f45a3c3a2326298d0ff711ad1
YanaMan <manaevaiana@gmail.com>
2021-01-27 21:34:19 +0300

    Add 8 and 9

---
+++ src/Kalkulator8.java
@@ -0,0 +1,9 @@
+public class Kalkulator8 {
+    public static void main(String[] args) {
+        int I = 5;
+        int IV = 1;
+        int result = I-IV;
+        System.out.println("Результат:"+result);
+    }
+}
+
---
+++ src/Kalkulator9.java
@@ -0,0 +1,17 @@
+public class Kalkulator9 {
+    public static void main(String[] args) {
+    int I = 1;
+    int II = 2;
+    int III = 3;
+    int IV = 4;
+    int V = 5;
+    int VI = 6;
+    int VII = 7;
+    int VIII = 8;
+    int IX = 9;
+    int X = 10;
+    int result = I+VII;
+    System.out.println("Результат:"+result);
+}
+}
+
